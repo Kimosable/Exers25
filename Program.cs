ff01003785b04203ac588827ef21555f700466fb
Kimosable <129333154+Kimosable@users.noreply.github.com>
2023-04-28 14:37:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+System.Console.WriteLine("Введите первое число ");
+int number1 = int.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Введите второе число ");
+int number2 = int.Parse(System.Console.ReadLine());
+
+int Quatro(int number3)
+{
+int summ = 1;
+for(int i = 0; i != number2; i++)
+{
+    summ = summ * number1;
+}
+return summ;
+}
+
+System.Console.WriteLine(Quatro(number1));
